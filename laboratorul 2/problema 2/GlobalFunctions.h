a5a215bf5f3947d731e99027ec814aa9eef37377
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2021-02-25 17:16:49 +0200

    Update GlobalFunctions.h

--- laboratorul 2/problema 2/GlobalFunctions.h
+++ laboratorul 2/problema 2/GlobalFunctions.h
@@ -3,12 +3,8 @@
 
 inline bool between(float grade);
 
-int nameComparison(Student s1, Student s2);//compare the names
-
-int mathGradeComparison(Student s1, Student s2);//compare the math grade
-
-int englishGradeComparison(Student s1, Student s2);//compare the english grade
-
-int historyGradeComparison(Student s1, Student s2);//compare the history grade
-
-int averageComparison(Student s1, Student s2);//calculate the average grade
+int nameComparison(Student& s1, Student& s2);
+int mathGradeComparison(Student& s1, Student& s2);
+int englishGradeComparison(Student& s1, Student& s2);
+int historyGradeComparison(Student& s1, Student& s2);
+int averageComparison(Student& s1, Student& s2);
