7c856590483ac44c818dc405843855a8dacf6cbe
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2021-04-15 17:12:49 +0300

    Update Source.cpp

--- laboratorul 8/Source.cpp
+++ laboratorul 8/Source.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 int main()
 {
+    map<string, int>aux;
     map<string, int> myMap;
     auto comp = [&](const string& a, const string& b) -> bool {
         if (myMap[a] == myMap[b])
@@ -16,14 +17,16 @@ int main()
         else
             return myMap[a] < myMap[b];
     };
+    auto comp2 = [&](const string& a, const string& b) -> bool {
+        return a.size() < b.size();
+    };
     priority_queue<string, vector<string>, decltype(comp)> myQueue(comp);
+    priority_queue<string, vector<string>, decltype(comp2)> myQueue2(comp2);
     string input;
     getline(cin, input);
     for (int i = 0; i <= input.length(); ++i)
         if (input[i] >= 'A' && input[i] <= 'Z')
             input[i] = input[i] - 'A' + 'a';
-
-    map<string, int> aux;
     istringstream iss(input);
     int i = 0;
     while (iss)
@@ -48,15 +51,16 @@ int main()
         int pos = input.find(word, 0);
         while (pos != string::npos)
         {
-            if ((pos < input.length() && input[pos + 1] >= 'a' && input[pos + 1] <= 'z'))
+            if (pos < input.length() && (input[pos - 1] < 'a' || input[pos - 1] > 'z'))
                 ++count;
             pos = input.find(word, pos + 1);
         }
         if (word.size() != 0)
         {
             myMap[word] = count;
+            aux[word] = count;
             myQueue.push(word);
-            word = " ";
+            myQueue2.push(word);
         }
     }
     while (!myQueue.empty())
@@ -65,6 +69,14 @@ int main()
             cout << myQueue.top() << " => " << myMap[myQueue.top()] << '\n';
         myMap[myQueue.top()] = -112;
         myQueue.pop();
+    }
+    cout << "\n---------------------------------------------------------------\n";
+    while (!myQueue2.empty())
+    {
+        if (aux[myQueue2.top()] != -112)
+            cout << myQueue2.top() << " => " << aux[myQueue2.top()] << '\n';
+        aux[myQueue.top()] = -112;
+        myQueue2.pop();
     }
 
     return 0;
